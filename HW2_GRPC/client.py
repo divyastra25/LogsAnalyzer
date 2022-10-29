bdc0d615cf36ddea505ebb454544631c190bc377
Divya Patel <divya@Divyas-MacBook-Air.local>
2022-10-29 00:15:41 -0500

    added logging for gRPC

--- HW2_GRPC/client.py
+++ HW2_GRPC/client.py
@@ -1,15 +1,32 @@
+from http import client
 import grpc
 import logs_pb2
 import logs_pb2_grpc
+import logging
+
+logging.basicConfig(filename="logs/client_logs.log", level=logging.NOTSET)
+# from configparser import ConfigParser
+
+# configFile = "config.ini"
+# config = ConfigParser()
+# config.read(configFile)
+
+# startTime = str(config['client']['start'])
+# deltaTime = str(config['client']['delta'])
 
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = logs_pb2_grpc.LogRetrieverStub(channel)
+        logging.info("Client stub created!")
+        
+        get_msg_req = logs_pb2.GetMessageReq(start="17:44:33", delta="00:01:02")
+        logging.info("GetMessageReq Sent!")
 
-        get_msg_req = logs_pb2.GetMessageReq(start="08:16:46", delta="00:00:24")
         logs_reply = stub.GetMessages(get_msg_req)
-        print("GetMessages response receieved!")
+        logging.info("GetMessages response receieved!")
+        logging.info(logs_reply)
         print(logs_reply)
 
 if __name__ == "__main__":
+    logging.info("Running the client")
     run()
--- HW2_GRPC/server.py
+++ HW2_GRPC/server.py
@@ -3,18 +3,24 @@ import logs_pb2
 import logs_pb2_grpc
 import requests
 from concurrent import futures
+import logging
+
+logging.basicConfig(filename="logs/server_logs.log", level=logging.NOTSET)
 
 class LogRetrieverService(logs_pb2_grpc.LogRetrieverServicer):
     def GetMessages(self, request, context):
         print("GetMessages request received!")
+        logging.info("GetMessages request received!")
         endpoint = f"https://qnvqyz4nka.execute-api.us-east-2.amazonaws.com/test/indexes?start={request.start}&delta={request.delta}"
         results = requests.get(endpoint)
+        print("results received")
+        logging.info("Results Received!")
 
         logs_reply = logs_pb2.MessagesReply()
         logs_reply.startIndex = results.json()["startIndex"]
         logs_reply.endIndex = results.json()["endIndex"]
         
-        
+        logging.info("Sending Reply")
         return logs_reply
 
 def serve():
@@ -22,7 +28,9 @@ def serve():
     logs_pb2_grpc.add_LogRetrieverServicer_to_server(LogRetrieverService(), server)
     server.add_insecure_port("localhost:50051")
     server.start()
+    logging.info("Server Started!")
     server.wait_for_termination()
 
 if __name__ == "__main__":
+    logging.info("Starting serve()")
     serve()
